3c21d4f8687685ccff1943476a328bad99b265b9
Rizal Aji Santoso <148060269+rizalajisantoso@users.noreply.github.com>
2024-03-30 20:43:40 +0700

    Update sql_script.sql

--- sql_script.sql
+++ sql_script.sql
@@ -1,7 +1,20 @@
-select orders.date as order_date,product_category.categoryname as category_name,product.prodname as product_name,product.price as product_price,orders.quantity as order_qty,(product.price*orders.quantity) as total_sales,customers.customeremail as cust_email,customers.customercity as cust_city
+select 
+  orders.date as order_date,
+  product_category.categoryname as category_name,
+  product.prodname as product_name,
+  product.price as product_price,
+  orders.quantity as order_qty,
+  (product.price*orders.quantity) as total_sales,
+  customers.customeremail as cust_email,
+  customers.customercity as cust_city
+  
 from customers 
 inner join orders on customers.customerid = orders.CustomerID
 inner join product on orders.prodnumber = product.ProdNumber
 inner join product_category on product.category = product_category.categoryid
-order by order_date asc,order_qty asc,total_sales asc;
+  
+order by 
+  order_date asc,
+  order_qty asc,
+  total_sales asc;
 
